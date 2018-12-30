109d20ccc8c941174f27454967c65c228f2edcd7
Sava Lazic <savalazic9@gmail.com>
2018-12-30 01:55:16 +0100

    implement basic event bus

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,19 @@
-describe('index', () => {
-  it('should be true', () => {
-    expect(true).toBe(true);
+import { EventBus } from './';
+
+describe('EventBus', () => {
+  let eventBus: any = {};
+
+  beforeEach(() => {
+    eventBus = new EventBus();
+  });
+
+  it('should works with basic usage', () => {
+    let value = 1;
+    eventBus.addEventListener(
+      'increment',
+      ({ detail }: any) => (value += detail.value),
+    );
+    eventBus.dispatchEvent('increment', { value: 5 });
+    expect(value).toEqual(6);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
-const test = 'setup';
+export class EventBus {
+  public eventBus: HTMLElement;
 
-export function func() {
-  return test;
+  /**
+   *  initialize a new EventBus instance
+   */
+  constructor() {
+    this.eventBus = document.createElement('eventBus');
+  }
+
+  /**
+   * Add an event listener
+   */
+  public addEventListener(event: string, callback: () => void) {
+    this.eventBus.addEventListener(event, callback);
+  }
+
+  /**
+   * Remove an event listener
+   */
+  public removeEventListener(event: string, callback: () => void) {
+    this.eventBus.removeEventListener(event, callback);
+  }
+
+  /**
+   * Dispatch an event
+   */
+  public dispatchEvent(event: string, detail = {}) {
+    this.eventBus.dispatchEvent(new CustomEvent(event, { detail }));
+  }
 }
 
-func();
+(window as any).EventBus = new EventBus();
